29f544823e554e4aec369525d629b7848e3645a3
Ol_Kuts <kuts.ol.vas@gmail.com>
2023-01-24 07:17:36 +0200

    init

---
+++ ts-react-sass-eslint-prettier/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import { Component } from './Component'
+
+export const App: FC = () => {
+  return (
+    <>
+      <Component />
+    </>
+  )
+}
---
+++ ts-react-sass-eslint-prettier/src/components/Component.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react'
+
+export const Component: FC = () => {
+  return <div>hello my start</div>
+}
